4e92cfe702864c7c4df9f640224aa9bdbb21b432
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-09-30 19:06:42 +0200

    minor changes and checked baxos performance graph

--- consenbench/controller/attack_basic.go
+++ consenbench/controller/attack_basic.go
@@ -21,47 +21,17 @@ func (a *BasicAttack) Attack(nodes []*AttackNode, links [][]*AttackLink, oracle 
 	start_time := time.Now()
 
 	for time.Now().Sub(start_time).Seconds() < float64(duration-5) {
-		links[0][1].SetBandwidth(1000)
-		links[0][2].SetBandwidth(2000)
-
 		links[0][1].SetDelay(100)
-		links[0][2].SetDelay(200)
-
-		//links[1][2].SetBandwidth(3)
-		//links[1][3].SetBandwidth(4)
-		//
-		//links[2][3].SetBandwidth(5)
-		//links[2][4].SetBandwidth(6)
-		//
-		//links[3][4].SetBandwidth(7)
-		//links[3][0].SetBandwidth(8)
-		//
-		//links[4][0].SetBandwidth(9)
-		//links[4][1].SetBandwidth(10)
-
-		time.Sleep(3 * time.Second)
-
-		links[0][1].SetBandwidth(10000000)
-		links[0][2].SetBandwidth(10000000)
-
-		links[0][1].SetDelay(0)
-		links[0][2].SetDelay(0)
-
-		//links[1][2].SetBandwidth(10000000)
-		//links[1][3].SetBandwidth(10000000)
-		//
-		//links[2][3].SetBandwidth(10000000)
-		//links[2][4].SetBandwidth(10000000)
-		//
-		//links[3][4].SetBandwidth(10000000)
-		//links[3][0].SetBandwidth(10000000)
-		//
-		//links[4][0].SetBandwidth(10000000)
-		//links[4][1].SetBandwidth(10000000)
+		links[1][2].SetDelay(200)
+		links[2][0].SetDelay(300)
 
 		time.Sleep(1 * time.Second)
-		//fmt.Printf("The leader order is %v\n", oracle.GetTopNLeaders())
-		//time.Sleep(1 * time.Second)
+
+		links[0][1].SetDelay(0)
+		links[1][2].SetDelay(0)
+		links[2][0].SetDelay(0)
+
+		time.Sleep(2 * time.Second)
 	}
 
 	fmt.Print("Basic attack complete\n")
