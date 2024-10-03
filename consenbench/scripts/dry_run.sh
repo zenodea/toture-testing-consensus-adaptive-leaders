d7a34aad7e053f21f7bf89df00abeb09f7e81e62
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-10-03 13:45:45 +0200

    checked dry_script.sh; checked

--- consenbench/scripts/dry_run.sh
+++ consenbench/scripts/dry_run.sh
@@ -1,8 +1,17 @@
 algo=$1
-attack=$2
 
 git pull origin main
 /bin/bash build.sh
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type bootstrap  --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device enp1s0
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type copy       --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device enp1s0
-./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type bootstrap  --consensus_algorithm ${algo}  --attack_duration 60 --attack noop --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type copy       --consensus_algorithm ${algo}  --attack_duration 60 --attack noop --device enp1s0
+
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack basic         --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack noop          --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_1   --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_2   --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_3   --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_4_1 --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_4_2 --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_5   --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_6   --device enp1s0
+./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack partition_7   --device enp1s0
