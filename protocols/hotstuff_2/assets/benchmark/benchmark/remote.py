ac7ab697a00453debad28f744fd6b8947091cc33
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-10-21 16:59:36 +0200

    hotstuff_2 remote.py all files checked

--- protocols/hotstuff_2/assets/benchmark/benchmark/remote.py
+++ protocols/hotstuff_2/assets/benchmark/benchmark/remote.py
@@ -71,6 +71,7 @@ class Bench:
             "rustup default stable",
             # This is missing from the Rocksdb installer (needed for Rocksdb).
             "sudo apt-get install -y clang",
+            "sudo rm -r hotstuff"
             # Clone the repo.
             f"(git clone {self.settings.repo_url} || (cd {self.settings.repo_name} ; git pull))",
         ]
