9697602c13f271268245c297bbdf370279d1ba43
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-10-21 17:01:33 +0200

    hotstuff_2 remote.py all files checked

--- protocols/hotstuff_2/assets/benchmark/benchmark/remote.py
+++ protocols/hotstuff_2/assets/benchmark/benchmark/remote.py
@@ -62,17 +62,13 @@ class Bench:
             "sudo apt-get update",
             "sudo apt-get -y upgrade",
             "sudo apt-get -y autoremove",
-            # The following dependencies prevent the error: [error: linker `cc` not found].
             "sudo apt-get -y install build-essential",
             "sudo apt-get -y install cmake",
-            # Install rust (non-interactive).
             'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y',
             "source $HOME/.cargo/env",
             "rustup default stable",
-            # This is missing from the Rocksdb installer (needed for Rocksdb).
             "sudo apt-get install -y clang",
-            "sudo rm -r hotstuff"
-            # Clone the repo.
+            "sudo rm -r hotstuff",
             f"(git clone {self.settings.repo_url} || (cd {self.settings.repo_name} ; git pull))",
         ]
         hosts = self.manager.hosts()
