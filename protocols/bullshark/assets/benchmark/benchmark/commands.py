35e04375630c2ef6529d5c87a021257b8737e678
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-05-08 20:24:45 +0200

    bullshark added  building --target; all files checked

--- protocols/bullshark/assets/benchmark/benchmark/commands.py
+++ protocols/bullshark/assets/benchmark/benchmark/commands.py
@@ -18,7 +18,7 @@ class CommandMaker:
 
     @staticmethod
     def compile():
-        return 'cargo build --quiet --features benchmark'
+        return 'cargo build --quiet --release --features benchmark'
 
     @staticmethod
     def generate_key(filename):
