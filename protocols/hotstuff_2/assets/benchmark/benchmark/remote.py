3571222954ec023b5d4f36619b848c14656dcc19
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-05-08 21:44:52 +0200

    hotstuff_2 removed progress bar, moved building to bootstrap; all files checked

--- protocols/hotstuff_2/assets/benchmark/benchmark/remote.py
+++ protocols/hotstuff_2/assets/benchmark/benchmark/remote.py
@@ -72,6 +72,10 @@ class Bench:
             "sudo apt-get install -y clang",
             "sudo rm -r hotstuff",
             f"(git clone {self.settings.repo_url} || (cd {self.settings.repo_name} ; git pull))",
+            f"(cd {self.settings.repo_name} && git checkout -f {self.settings.branch})",
+            "source $HOME/.cargo/env",
+            f"(cd {self.settings.repo_name}/node && {CommandMaker.compile()})",
+            CommandMaker.alias_binaries(f"./{self.settings.repo_name}/target/release/"),
         ]
         hosts = self.manager.hosts()
         try:
@@ -113,14 +117,7 @@ class Bench:
 
     def _update(self, hosts):
         Print.info(f'Updating {len(hosts)} nodes (branch "{self.settings.branch}")...')
-        cmd = [
-            f"(cd {self.settings.repo_name} && git fetch -f)",
-            f"(cd {self.settings.repo_name} && git checkout -f {self.settings.branch})",
-            f"(cd {self.settings.repo_name} && git pull -f)",
-            "source $HOME/.cargo/env",
-            f"(cd {self.settings.repo_name}/node && {CommandMaker.compile()})",
-            CommandMaker.alias_binaries(f"./{self.settings.repo_name}/target/release/"),
-        ]
+        cmd = []
         g = Group(*hosts, user=self.manager.user(), connect_kwargs=self.connect)
         g.run(" && ".join(cmd), hide=True)
 
@@ -261,12 +258,12 @@ class Bench:
         else:
             Print.info(f"Selected {len(selected_hosts)} instances")
 
-        # Update nodes.
-        try:
-            self._update(selected_hosts)
-        except (GroupException, ExecutionError) as e:
-            e = FabricError(e) if isinstance(e, GroupException) else e
-            raise BenchError("Failed to update nodes", e)
+        # # Update nodes.
+        # try:
+        #     self._update(selected_hosts)
+        # except (GroupException, ExecutionError) as e:
+        #     e = FabricError(e) if isinstance(e, GroupException) else e
+        #     raise BenchError("Failed to update nodes", e)
 
         # Run benchmarks.
         for n in bench_parameters.nodes:
