7cff80fe63d64b0b04f2e47799f48f98831059d2
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-05-20 16:40:17 +0200

    modified and  checked dedis_paxos params

--- protocols/dedis_paxos/dedis_paxos.go
+++ protocols/dedis_paxos/dedis_paxos.go
@@ -175,7 +175,7 @@ func (ba *Dedis_Paxos) Bootstrap(nodes []*common.Node, duration int, result chan
 
 	for j := 0; j < int(num_replicas); j++ {
 		go func(i int) {
-			nodes[i].ExecCmd("." + replica_path + " --name " + strconv.Itoa(i+1) + " --consAlgo paxos " + " --viewTimeout " + view_timeout_time + " --pipelineLength " + pipeline_length + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + "  --replica_batch_size " + replica_batch_size + "  --replica_batch_time " + replica_batch_time + "  --key_len " + key_len + "  --val_len " + val_len)
+			nodes[i].ExecCmd("." + replica_path + " --name " + strconv.Itoa(i+1) + " --consAlgo paxos " + " --viewTimeout " + view_timeout_time + " --pipelineLength " + pipeline_length + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + "  --batchSize " + replica_batch_size + "  --batchTime " + replica_batch_time + "  --keyLen " + key_len + "  --valLen " + val_len)
 		}(j)
 	}
 
@@ -199,7 +199,7 @@ func (ba *Dedis_Paxos) Bootstrap(nodes []*common.Node, duration int, result chan
 	m := 1
 	for j := int(num_replicas); j < int(num_replicas+num_clients); j++ {
 		go func(i int, k int) {
-			clientOutputs[i-int(num_replicas)] = nodes[i].ExecCmd("." + ctl_path + " --name " + strconv.Itoa(50+k) + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + " --requestType request --arrivalRate  " + arrival_rate + " --testDuration " + strconv.Itoa(duration) + "  --client_batch_size " + client_batch_size + "  --client_batch_time " + client_batch_time + "  --key_len " + key_len + "  --val_len " + val_len + "  --client_window " + client_window)
+			clientOutputs[i-int(num_replicas)] = nodes[i].ExecCmd("." + ctl_path + " --name " + strconv.Itoa(50+k) + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + " --requestType request --arrivalRate  " + arrival_rate + " --testDuration " + strconv.Itoa(duration) + "  --batchSize " + client_batch_size + "  --batchTime " + client_batch_time + "  --keyLen " + key_len + "  --valLen " + val_len + "  --window " + client_window)
 		}(j, m)
 		m++
 	}
