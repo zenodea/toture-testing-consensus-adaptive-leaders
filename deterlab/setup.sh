81e5371db799412db50f9918f2445b4a65738d00
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-05-20 21:15:45 +0200

    modified  setup.sh; all files checked

--- deterlab/setup.sh
+++ deterlab/setup.sh
@@ -3,25 +3,7 @@
 NODES=$(seq 1 19)
 GO_VERSION="1.19"
 
-cmd=$(cat <<'EOF'
-set -e
-sudo apt update
-sudo apt install -y git iproute2 build-essential clang pkg-config libssl-dev python3 python3-pip openjdk-11-jdk
-sudo rm -rf /usr/local/go
-wget -q https://go.dev/dl/go'"${GO_VERSION}"'.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go'"${GO_VERSION}"'.linux-amd64.tar.gz
-rm go'"${GO_VERSION}"'.linux-amd64.tar.gz
-grep -qxF 'export PATH=$PATH:/usr/local/go/bin' ~/.bashrc || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-. ~/.bashrc
-sudo rm -rf ~/.cargo ~/.rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-sudo chmod u+s /usr/sbin/tc
-sudo setcap cap_net_admin,cap_net_raw+ep $(which tc)
-getcap $(which tc)
-pip3 install --user --no-cache-dir --force-reinstall fabric invoke decorator matplotlib boto3 etcd3 kazoo protobuf==3.19.6
-EOF
-)
+cmd="set -e;sudo apt update;sudo apt install -y git iproute2 build-essential clang pkg-config libssl-dev python3 python3-pip openjdk-11-jdk;sudo rm -rf /usr/local/go;wget -q https://go.dev/dl/go'${GO_VERSION}'.linux-amd64.tar.gz;sudo tar -C /usr/local -xzf go'${GO_VERSION}'.linux-amd64.tar.gz;rm go'${GO_VERSION}'.linux-amd64.tar.gz;export PATH=$PATH:/usr/local/go/bin; ~/.bashrc ;echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc;. ~/.bashrc;sudo rm -rf ~/.cargo ~/.rustup;curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y;. $HOME/.cargo/env;sudo chmod u+s /usr/sbin/tc;sudo setcap cap_net_admin,cap_net_raw+ep $(which tc);getcap $(which tc);pip3 install --user --no-cache-dir --force-reinstall fabric invoke decorator matplotlib boto3 etcd3 kazoo protobuf==3.19.6"
 
 for i in $NODES; do
     echo "Processing node_$i..."
