c6e0f8cedee7ab94d690d8cebdb01dd89fd44dbf
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-05-21 17:15:59 +0200

    added and checked all racs  params; all files checked

--- protocols/racs/racs.go
+++ protocols/racs/racs.go
@@ -123,6 +123,46 @@ func (ba *Racs) Bootstrap(nodes []*common.Node, duration int, result chan util.P
 		panic(err.Error() + " while parsing arrival_rate")
 	}
 
+	network_batch_time, ok := ba.options.Option["network_batch_time"]
+	if !ok {
+		panic(err.Error() + " while parsing network_batch_time")
+	}
+
+	replica_batch_size, ok := ba.options.Option["replica_batch_size"]
+	if !ok {
+		panic(err.Error() + " while parsing replica_batch_size")
+	}
+
+	replica_batch_time, ok := ba.options.Option["replica_batch_time"]
+	if !ok {
+		panic(err.Error() + " while parsing replica_batch_time")
+	}
+
+	key_len, ok := ba.options.Option["key_len"]
+	if !ok {
+		panic(err.Error() + " while parsing key_len")
+	}
+
+	val_len, ok := ba.options.Option["val_len"]
+	if !ok {
+		panic(err.Error() + " while parsing val_len")
+	}
+
+	client_batch_size, ok := ba.options.Option["client_batch_size"]
+	if !ok {
+		panic(err.Error() + " while parsing client_batch_size")
+	}
+
+	client_batch_time, ok := ba.options.Option["client_batch_time"]
+	if !ok {
+		panic(err.Error() + " while parsing client_batch_time")
+	}
+
+	client_window, ok := ba.options.Option["client_window"]
+	if !ok {
+		panic(err.Error() + " while parsing client_window")
+	}
+
 	var wg sync.WaitGroup
 	wg.Add(int(num_replicas + num_clients))
 	for i := 0; i < int(num_replicas+num_clients); i++ {
@@ -140,7 +180,7 @@ func (ba *Racs) Bootstrap(nodes []*common.Node, duration int, result chan util.P
 
 	for j := 0; j < int(num_replicas); j++ {
 		go func(i int) {
-			nodes[i].ExecCmd("." + replica_path + " --name " + strconv.Itoa(i+1) + " --viewTimeout " + view_timeout_time + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + " --pipelineLength " + pipeline_length + " --networkbatchTime 0")
+			nodes[i].ExecCmd("." + replica_path + " --name " + strconv.Itoa(i+1) + " --viewTimeout " + view_timeout_time + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + " --pipelineLength " + pipeline_length + " --networkbatchTime " + network_batch_time + " --batchSize " + replica_batch_size + " --batchTime  " + replica_batch_time + " --keyLen " + key_len + " --valLen  " + val_len)
 		}(j)
 	}
 
@@ -164,7 +204,7 @@ func (ba *Racs) Bootstrap(nodes []*common.Node, duration int, result chan util.P
 	m := 1
 	for j := int(num_replicas); j < int(num_replicas+num_clients); j++ {
 		go func(i int, k int) {
-			clientOutputs[i-int(num_replicas)] = nodes[i].ExecCmd("." + ctl_path + " --name " + strconv.Itoa(50+k) + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + " --requestType request --arrivalRate  " + arrival_rate + " --testDuration " + strconv.Itoa(duration))
+			clientOutputs[i-int(num_replicas)] = nodes[i].ExecCmd("." + ctl_path + " --name " + strconv.Itoa(50+k) + " --logFilePath " + fmt.Sprintf("%vbench/logs/", nodes[i].HomeDir) + " --config " + fmt.Sprintf("%vbench/ip_config.yaml", nodes[i].HomeDir) + " --requestType request --arrivalRate  " + arrival_rate + " --testDuration " + strconv.Itoa(duration) + " --keyLen " + key_len + " --valLen " + val_len + " --batchSize " + client_batch_size + " --batchTime " + client_batch_time + " --window " + client_window)
 		}(j, m)
 		m++
 	}
