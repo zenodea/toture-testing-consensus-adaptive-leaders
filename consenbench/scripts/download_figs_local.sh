6a396f9ca1e5ffcf752f264d2792349a66f9ecf6
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-05-27 17:38:43 +0200

    modified the download fig scripts to only download pdfs; all files checked

--- consenbench/scripts/download_figs_local.sh
+++ consenbench/scripts/download_figs_local.sh
@@ -1,3 +1,3 @@
 sudo rm -r logs
 mkdir logs
-scp -r -i ~/.ssh/merge_key -J mergejump pasindut@torture-pasindut:~/toture-testing-consensus/logs/  logs/
+scp -i ~/.ssh/merge_key -J mergejump pasindut@torture-pasindut:~/toture-testing-consensus/logs/*.pdf  logs/
