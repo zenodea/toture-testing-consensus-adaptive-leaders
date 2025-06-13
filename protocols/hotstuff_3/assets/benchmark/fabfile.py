b0e82826a89cd4406e84711d782b8a9fbe72ac08
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-13 15:37:11 +0200

    added view timeout 2s for all protocols; all files checked

--- protocols/hotstuff_3/assets/benchmark/fabfile.py
+++ protocols/hotstuff_3/assets/benchmark/fabfile.py
@@ -27,7 +27,7 @@ def remote(ctx, pid=1, attack_duration=2, num_replicas=3):
     }
     node_params = {
         'consensus': {
-            'timeout_delay': 5_000,
+            'timeout_delay': 2_000,
             'sync_retry_delay': 5_000,
             'max_payload_size': 1_000,
             'min_block_delay': 100
