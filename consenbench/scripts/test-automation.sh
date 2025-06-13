6c5f7ae63247ad78fb707c2f4e1c75e588439f68
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-13 15:46:44 +0200

    test-automation for noop checked

--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -6,6 +6,13 @@ mkdir logs
 
 echo "Running tests for all protocols..."
 
-/bin/bash  consenbench/scripts/dry_run.sh quepaxa noop eth1
-/bin/bash  consenbench/scripts/dry_run.sh dedis_paxos noop eth1
-/bin/bash  consenbench/scripts/dry_run.sh quepaxa leader_1 eth1
+/bin/bash  consenbench/scripts/dry_run.sh dedis_paxos   noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh dedis_raft    noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh sadl_racs     noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh racs          noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh efficient     noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh quepaxa       noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh hotstuff_2    noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh hotstuff_3    noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh etcd          noop eth1
+/bin/bash  consenbench/scripts/dry_run.sh zoo_keeper    noop eth1
