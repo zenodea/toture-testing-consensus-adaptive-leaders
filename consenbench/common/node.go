4d5761cbd9010feb04b0f31440a7ce57771f5627
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-15 22:57:48 +0200

    hotstuff 2 debug -- all files checked

--- consenbench/common/node.go
+++ consenbench/common/node.go
@@ -86,6 +86,8 @@ func (n *Node) Start_Client(device string) error {
 
 	fmt.Printf("Starting client on node: %v\n", n.Id)
 	n.ExecCmd("pkill -KILL -f bench")
+	n.ExecCmd("pkill -KILL -f fab")
+	exec.Command("pkill", "fab")
 
 	if n.Logger.DebugOn {
 		go n.ExecCmd(fmt.Sprintf("./bench/bench --node_config %vbench/ip.yaml --id %v --debug_on --debug_level %v --device %v ", n.HomeDir, n.Id, n.Logger.Level, device))
