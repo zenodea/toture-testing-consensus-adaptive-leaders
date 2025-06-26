ba247f69d6ec554d24d16f38f23c48353d235209
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-26 20:02:22 +0200

    added all protocols to test script -- all files checked

--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -6,8 +6,9 @@ mkdir logs
 
 echo "Running tests for all protocols..."
 
-for protocol in dedis_paxos; do
+for protocol in dedis_paxos dedis_raft rabia sadl_racs racs efficient quepaxa mahi mysticeti hotstuff_2 hotstuff_3 tusk bullshark etcd zoo_keeper; do
   for attack in noop LeaderPartition OneQuorumNodePartition MajorityHighDelay MinorityCrash MinorityStraggler; do
+    echo "..........Running ${protocol} ${attack}........"
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1
   done
 done
