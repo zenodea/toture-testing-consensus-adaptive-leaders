2c0ca5946e6145372cbee4f1503abf49bc429165
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-27 20:37:34 +0200

    set load to 5k  -- all files checked

--- protocols/bullshark/assets/benchmark/fabfile.py
+++ protocols/bullshark/assets/benchmark/fabfile.py
@@ -23,7 +23,7 @@ def remote(ctx, pid=1, attack_duration=2, num_replicas=3, debug=False):
         'nodes': [int(num_replicas)],
         'workers': 1,
         'collocate': True,
-        'rate': [50_000],
+        'rate': [5000],
         'tx_size': 18,
         'duration': int(attack_duration),
         'runs': 1,
--- protocols/hotstuff_3/assets/benchmark/fabfile.py
+++ protocols/hotstuff_3/assets/benchmark/fabfile.py
@@ -19,7 +19,7 @@ def remote(ctx, pid=1, attack_duration=2, num_replicas=3):
     ''' Run benchmarks on AWS '''
     bench_params = {
         'nodes': [int(num_replicas)],
-        'rate': [50_000],
+        'rate': [5000],
         'tx_size': 18,
         'faults': 0,
         'duration': int(attack_duration),
