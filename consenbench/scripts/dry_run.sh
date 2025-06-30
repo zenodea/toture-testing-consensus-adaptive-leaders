bfb4fd166e30fd19c30d6a83c3492d1a256b6ee1
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-30 16:39:02 +0200

    script formatting changes, all files checked

--- consenbench/scripts/dry_run.sh
+++ consenbench/scripts/dry_run.sh
@@ -2,7 +2,6 @@ algo=$1
 attack=$2
 device=$3
 
-/bin/bash build.sh
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type bootstrap  --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type copy       --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 5 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -4,11 +4,12 @@ rm -r logs
 mkdir final-results
 mkdir logs
 
+/bin/bash build.sh
+
 echo "Running tests for all protocols..."
 
 for protocol in dedis_paxos dedis_raft rabia sadl_racs racs efficient quepaxa mahi mysticeti hotstuff_2 hotstuff_3 tusk bullshark etcd zoo_keeper; do
   for attack in noop; do
-    echo "..........Running ${protocol} ${attack}........"
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1
   done
 done
