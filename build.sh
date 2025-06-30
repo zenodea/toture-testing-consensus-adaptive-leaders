b1c4c844bc8e0653ffd99f174b5004cb761e5764
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-30 16:37:18 +0200

    changed build.sh to add all  output, all files checked

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-/usr/local/go/bin/go install google.golang.org/protobuf/cmd/protoc-gen-go@latest > /dev/null 2>&1
-/usr/local/go/bin/go get github.com/shirou/gopsutil/v3/... > /dev/null 2>&1
-/usr/local/go/bin/go get fyne.io/fyne/v2 > /dev/null 2>&1
-export PATH=$PATH:/usr/local/go/bin > /dev/null 2>&1
-/usr/local/go/bin/go mod vendor > /dev/null 2>&1
-/usr/local/go/bin/go mod tidy > /dev/null 2>&1
-protoc --go_out=. --go_opt=paths=source_relative consenbench/common/message.proto > /dev/null 2>&1
-/usr/local/go/bin/go build -v -o ./consenbench/bin/bench ./consenbench/ > /dev/null 2>&1
+pwd
+/usr/local/go/bin/go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+/usr/local/go/bin/go get github.com/shirou/gopsutil/v3/...
+/usr/local/go/bin/go get fyne.io/fyne/v2
+export PATH=$PATH:/usr/local/go/bin
+/usr/local/go/bin/go mod vendor
+/usr/local/go/bin/go mod tidy
+protoc --go_out=. --go_opt=paths=source_relative consenbench/common/message.proto
+/usr/local/go/bin/go build -v -o ./consenbench/bin/bench ./consenbench/
