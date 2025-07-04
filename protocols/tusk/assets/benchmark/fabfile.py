34d8208f37f8d6504da5818df4c0f52ea0b4a342
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-04 16:12:32 +0200

    set tusk header delay to 2s

--- protocols/tusk/assets/benchmark/fabfile.py
+++ protocols/tusk/assets/benchmark/fabfile.py
@@ -31,7 +31,7 @@ def remote(ctx, pid=1, attack_duration=2, num_replicas=3, debug=False):
     }
     node_params = {
         'header_size': 1_000,  # bytes
-        'max_header_delay': 200,  # ms
+        'max_header_delay': 2000,  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 10_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
