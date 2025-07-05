7ddea79b310846287a2316e8030bf7eae70c0232
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-05 16:24:50 +0200

    set test automation params to bft ones only

--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -9,8 +9,8 @@ mkdir logs
 echo "Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
 
-for protocol in dedis_paxos dedis_raft rabia sadl_racs racs efficient quepaxa mahi mysticeti hotstuff_2 hotstuff_3 tusk bullshark etcd zoo_keeper cft_dag; do
-  for attack in noop LeaderPartition OneQuorumNodePartition MajorityHighDelay MinorityCrash MinorityStraggler; do
+for protocol in mahi mysticeti hotstuff_2 hotstuff_3 tusk bullshark; do
+  for attack in noop; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1
   done
 done
