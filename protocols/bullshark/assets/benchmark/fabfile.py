84a96300fc9ee3afa60f4e3ba6b01d57fb3c2527
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-07 15:48:56 +0300

    set tusk and bullshark max header delay timeouts to defaults;  all files checked

--- protocols/bullshark/assets/benchmark/fabfile.py
+++ protocols/bullshark/assets/benchmark/fabfile.py
@@ -30,7 +30,7 @@ def remote(ctx, pid=1, attack_duration=2, num_replicas=3, debug=False):
     }
     node_params = {
         'header_size': 50,  # bytes
-        'max_header_delay': 10_000,  # ms
+        'max_header_delay': 1_000,  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 10_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
