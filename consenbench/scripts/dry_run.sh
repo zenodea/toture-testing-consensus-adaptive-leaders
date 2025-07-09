80765de1642c04e0eaa3c9ba60e0fd68a0dee1c8
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-09 09:56:04 +0300

    load  and size printing with comma;

--- consenbench/scripts/dry_run.sh
+++ consenbench/scripts/dry_run.sh
@@ -6,7 +6,7 @@ size=$5
 
 echo "$load $size" > params.param
 
-echo -n "$load $size "
+echo -n "$load, $size,"
 
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type bootstrap  --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type copy       --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
