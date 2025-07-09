d575bdc6ec0b679e9f6fa7fac3b53e5cb617884b
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-09 16:56:09 +0300

    updated test automation; all files checked;

--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -9,8 +9,12 @@ mkdir logs
 echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
 
-for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
+for protocol in mahi mysticeti hotstuff_2 tusk bullshark cft_dag; do
   for attack in noop; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1 60000 256
+    for size in 32 512; do
+      for load in 10000 20000 50000 70000 100000 120000 150000 180000 200000 250000 300000; do
+        /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1 "$load" "$size"
+      done
+    done
   done
 done
