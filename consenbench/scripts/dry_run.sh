dc3f041e136ca9b61f6d8bdd85b417f24bb45f49
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-09 09:39:16 +0300

    added load  and size printing to dry run;

--- consenbench/scripts/dry_run.sh
+++ consenbench/scripts/dry_run.sh
@@ -6,6 +6,8 @@ size=$5
 
 echo "$load $size" > params.param
 
+echo -n "$load $size "
+
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type bootstrap  --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type copy       --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type run        --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
