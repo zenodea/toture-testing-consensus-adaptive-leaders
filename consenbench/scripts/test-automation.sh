fe4c57a53114679b8b4a6c8632b4bcec029865c4
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-13 16:09:43 +0300

    broke setup and run in to 2 seperate dry_scripts; all files checked;

--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -9,9 +9,10 @@ mkdir logs
 echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
 for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
+  /bin/bash  consenbench/scripts/dry_setup.sh   "${protocol}" "${attack}" eth1 "$load" "$size"
   for attack in noop; do
-    for size in 32 512; do
-      for load in 500 5000 10000 20000 50000 80000 100000 120000 150000 180000 200000 230000 250000 300000 350000 400000 450000 500000 600000; do
+    for size in 32; do
+      for load in 500 5000; do
         /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1 "$load" "$size"
       done
     done
