7e7d73fb2b750c1fe75a20398ce2c2fd68f9daaa
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-13 21:28:16 +0300

    fixed setup.sh; all files checked;

--- consenbench/scripts/dry_setup.sh
+++ consenbench/scripts/dry_setup.sh
@@ -1,8 +1,6 @@
 algo=$1
 attack=$2
 device=$3
-load=$4
-size=$5
 
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type bootstrap  --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
 ./consenbench/bin/bench --is_controller --id 1 --debug_on --debug_level 0 --controller_operation_type copy       --consensus_algorithm ${algo}  --attack_duration 60 --attack ${attack} --device ${device}
--- consenbench/scripts/test-automation.sh
+++ consenbench/scripts/test-automation.sh
@@ -9,7 +9,7 @@ mkdir logs
 echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
 for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
-  /bin/bash  consenbench/scripts/dry_setup.sh   "${protocol}" "${attack}" eth1 "$load" "$size"
+  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
   for attack in noop; do
     for size in 32; do
       for load in 500 5000; do
