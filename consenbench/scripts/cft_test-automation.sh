fbad8996d4b1b53674289841567f25b0e3e31abc
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-15 12:35:11 +0200

    updated cft tests

--- consenbench/scripts/cft_test-automation.sh
+++ consenbench/scripts/cft_test-automation.sh
@@ -8,11 +8,11 @@ mkdir logs
 
 echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
-for protocol in dedis_paxos rabia sadl_racs efficient quepaxa cft_dag; do
+for protocol in cft_dag dedis_paxos rabia sadl_racs efficient quepaxa; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
   for attack in noop; do
     for size in 18; do
-      for load in 500 5000; do
+      for load in 10000 100000; do
         /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" eth1 "$load" "$size"
       done
     done
