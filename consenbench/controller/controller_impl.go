4c44f864fb32a2c3adf90fa64892ffa3be197739
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-19 23:07:03 +0100

    setup.sh only for 1--; all files checked

--- consenbench/controller/controller_impl.go
+++ consenbench/controller/controller_impl.go
@@ -23,7 +23,7 @@ func (c *Controller) SetupMachines() error {
 
 	var wg sync.WaitGroup
 	wg.Add(len(c.Nodes))
-	for j := 0; j < len(c.Nodes); j++ {
+	for j := 1; j < len(c.Nodes); j++ {
 		go func(i int) {
 
 			cmd := "set -e;sudo apt -y update;sudo apt-mark hold grub-efi-amd64 grub-pc grub-common; sudo apt -y upgrade;sudo apt-get -y autoremove; sudo apt install -y git iproute2 cmake build-essential clang pkg-config libssl-dev python3 python3-pip openjdk-11-jdk;sudo rm -rf /usr/local/go; wget -q https://go.dev/dl/go1.19.linux-amd64.tar.gz;sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz;rm go1.19.linux-amd64.tar.gz;echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc;. ~/.bashrc;export PATH=$PATH:/usr/local/go/bin:$HOME/.cargo/bin; sudo rm -rf ~/.cargo ~/.rustup;curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y;. $HOME/.cargo/env;rustup default stable; sudo chmod u+s /usr/sbin/tc;sudo setcap cap_net_admin,cap_net_raw+ep $(which tc);getcap $(which tc);pip3 install --user --no-cache-dir --force-reinstall fabric invoke decorator matplotlib boto3 etcd3 kazoo protobuf==3.19.6"
