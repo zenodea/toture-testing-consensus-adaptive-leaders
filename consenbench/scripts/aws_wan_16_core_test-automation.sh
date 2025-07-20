b87b987d1e2f19b3ed574c5535a277dbeeb687ac
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-20 11:09:56 +0100

    modified aws automation

--- consenbench/scripts/aws_wan_16_core_test-automation.sh
+++ consenbench/scripts/aws_wan_16_core_test-automation.sh
@@ -8,14 +8,20 @@ mkdir logs
 
 echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
-#for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
-for protocol in mahi; do
+for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
-  for attack in noop; do
-    for size in 512; do
-      for load in 100000; do
-        /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" "${attack}" ens5 "$load" "$size"
-      done
+  for size in 512; do
+    for load in 100000; do
+      /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
+    done
+  done
+done
+
+for protocol in dedis_paxos rabia sadl_racs efficient quepaxa cft_dag; do
+  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
+  for size in 18; do
+    for load in 100000; do
+      /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
     done
   done
 done
