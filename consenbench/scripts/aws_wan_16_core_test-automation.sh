8ec80a319e56cd32e4f392c098ba3bb14e24f209
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-20 18:50:18 +0100

    updated test automation; all files checked

--- consenbench/scripts/aws_wan_16_core_test-automation.sh
+++ consenbench/scripts/aws_wan_16_core_test-automation.sh
@@ -11,7 +11,7 @@ echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM
 for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
   for size in 512; do
-    for load in 30000; do
+    for load in 20000; do
       /bin/bash  consenbench/scripts/dry_run.sh  "${protocol}" noop ens5 "$load" "$size"
     done
   done
