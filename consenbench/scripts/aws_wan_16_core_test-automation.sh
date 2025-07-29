c8ad00184ad3ee689cbeb361b6984f60f2a52b37
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-07-29 11:52:59 +0200

    set cft nodes to 3, changed script; all files checked

--- consenbench/scripts/aws_wan_16_core_test-automation.sh
+++ consenbench/scripts/aws_wan_16_core_test-automation.sh
@@ -8,38 +8,10 @@ mkdir logs
 
 echo "Load,Size,Protocol,Attack,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
-for protocol in mahi mysticeti hotstuff_2 tusk bullshark; do
-  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
-  size=32
-  for load in 10000 100000 200000 350000 400000 500000 600000; do
-    /bin/bash  consenbench/scripts/dry_run.sh  "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=512
-  for load in 10000 50000 80000 100000 120000 150000 200000; do
-    /bin/bash  consenbench/scripts/dry_run.sh  "${protocol}" noop ens5 "$load" "$size"
-  done
-done
-
-for protocol in dedis_paxos sadl_racs quepaxa cft_dag; do
+for protocol in cft_dag dedis_paxos sadl_racs quepaxa efficient; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
   size=18
-  for load in 10000 100000 150000 200000 250000 300000 400000 500000 600000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=32
-  for load in 10000 50000 100000 150000 200000 250000 300000 400000 500000 600000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=512
-  for load in 5000 10000 20000 30000 40000 50000 100000 150000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-done
-
-for protocol in efficient; do
-  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
-  size=18
-  for load in 10000 100000 200000 300000 400000 500000 600000; do
+  for load in 10000 100000 200000 250000 300000 400000 500000 600000 800000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
   done
 done
