452081d2f121e8487eb1030b397875b2857f1296
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-08-01 11:41:35 +0200

    updated aws script,  files checked

--- consenbench/scripts/aws_wan_16_core_test-automation.sh
+++ consenbench/scripts/aws_wan_16_core_test-automation.sh
@@ -8,10 +8,10 @@ mkdir logs
 
 echo "Load,Size,Protocol,Attack,Num_Replicas,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
-for protocol in cft_dag dedis_paxos quepaxa sadl_racs efficient; do
+for protocol in cft_dag dedis_paxos quepaxa sadl_racs efficient rabia; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
   size=18
-  for load in 50000; do
+  for load in 10000 100000 200000 300000 400000 500000 600000 700000 800000 900000 10000000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" LeaderCrash ens5 "$load" "$size"
   done
 done
