3e119b6f0bfab82cf8cfb1161ba3083d0cd42507
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-08-04 10:55:49 +0200

    updated aws script,  files checked

--- consenbench/scripts/aws_wan_16_core_test-automation.sh
+++ consenbench/scripts/aws_wan_16_core_test-automation.sh
@@ -11,39 +11,7 @@ echo "Load,Size,Protocol,Attack,Num_Replicas,Throughput,Latency(ms),Percentile_9
 for protocol in cft_dag; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
   size=18
-  for load in 10000 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
+  for load in 700000 800000 900000 1000000 1200000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=32
-  for load in 10000 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=512
-    for load in 10000 20000 30000 80000 100000; do
-      /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-done
-
-for protocol in efficient; do
-  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
-  size=18
-  for load in 10000 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-done
-
-for protocol in dedis_paxos quepaxa dedis_raft sadl_racs racs; do
-  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop ens5
-  size=18
-  for load in 10000 50000 100000 200000 300000 400000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=32
-  for load in 10000 50000 100000 200000 300000 400000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
-  done
-  size=512
-    for load in 10000 20000 30000 50000; do
-      /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop ens5 "$load" "$size"
   done
 done
