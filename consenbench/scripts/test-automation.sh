b949811d63d836281bb26eb7afdba508281541ab
pasindutennage <pasindunivanthakatennage@gmail.com>
2025-08-16 22:17:56 +0200

    updated test script

---
+++ consenbench/scripts/test-automation.sh
@@ -0,0 +1,25 @@
+rm -r final-results
+rm -r logs
+
+mkdir final-results
+mkdir logs
+
+/bin/bash build.sh
+
+echo "Load,Size,Protocol,Attack,Num_Replicas,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
+
+for protocol in tusk bullshark hotstuff_2; do
+  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
+  size=18
+  for load in 10000 50000 100000 150000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
+    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
+  done
+  size=32
+  for load in 10000 50000 100000 150000 200000 300000 400000 500000 600000 700000 800000 900000 1000000; do
+    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
+  done
+  size=512
+  for load in 10000 20000 30000 50000 80000 100000 120000 150000 180000 200000 250000; do
+    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
+  done
+done
