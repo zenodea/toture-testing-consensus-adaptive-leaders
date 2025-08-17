bd759973867603459915902a7b1f3308c0fb91e9
pasindutennage <pasindunivanthakatennage@gmail.com>
2025-08-17 15:43:45 +0200

    registered EPaxos exec

--- consenbench/controller/controller_register.go
+++ consenbench/controller/controller_register.go
@@ -7,6 +7,7 @@ import (
 	dedis_paxos "toture-test/protocols/dedis_paxos"
 	dedis_raft "toture-test/protocols/dedis_raft"
 	efficient "toture-test/protocols/efficient"
+	efficient_exec "toture-test/protocols/efficient-exec"
 	etcd "toture-test/protocols/etcd"
 	hotstuff_2 "toture-test/protocols/hotstuff_2"
 	hotstuff_3 "toture-test/protocols/hotstuff_3"
@@ -60,6 +61,8 @@ func (c *Controller) GetProtocolImpl(protocol string) protocols.Consensus {
 		return racs.NewRacs(c.logger)
 	} else if protocol == "efficient" {
 		return efficient.NewEfficient(c.logger)
+	} else if protocol == "efficient-exec" {
+		return efficient_exec.NewEfficientExec(c.logger)
 	} else if protocol == "quepaxa" {
 		return quepaxa.NewQuePaxa(c.logger)
 	} else if protocol == "mahi" {
