0e04ef374fb63351c1e81e1043936422e8740238
pasindutennage <pasindunivanthakatennage@gmail.com>
2025-08-19 12:38:51 +0200

    bft test updated

--- consenbench/scripts/bft-test-automation.sh
+++ consenbench/scripts/bft-test-automation.sh
@@ -11,7 +11,7 @@ echo "Load,Size,Protocol,Attack,Num_Replicas,Throughput,Latency(ms),Percentile_9
 for protocol in tusk bullshark hotstuff_2; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
   size=32
-  for load in 10000 20000 50000 80000 100000 150000 200000 300000 400000; do
+  for load in 10000 20000 50000 80000 100000 120000 150000 200000 250000 300000 350000 400000 500000 600000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
   done
 done
