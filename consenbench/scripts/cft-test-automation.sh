7903802e37aa3eb83a1ee543bb9164d20d037a3d
pasindutennage <pasindunivanthakatennage@gmail.com>
2025-08-19 12:54:05 +0200

    cft test updated; pipeline set to 1

--- consenbench/scripts/cft-test-automation.sh
+++ consenbench/scripts/cft-test-automation.sh
@@ -12,7 +12,7 @@ for protocol in dedis_paxos dedis_raft  racs quepaxa; do # sadl_racs
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
 
   size=18
-  for load in 1000 10000 20000 50000 80000 100000 120000 150000 200000 250000 300000 400000; do
+  for load in 1000 10000 20000 50000 80000 100000 120000 150000 180000 200000 220000 250000 300000 350000 400000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
   done
 #
@@ -32,7 +32,7 @@ for protocol in efficient; do
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
 
   size=18
-  for load in 1000 20000 30000 50000 100000 120000 150000 200000 250000 300000 350000 400000; do
+  for load in 1000 10000 20000 50000 80000 100000 120000 150000 180000 200000 220000 250000 300000 350000 400000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
   done
 
