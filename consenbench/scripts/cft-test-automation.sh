0b5e701f85e43dc0fe24bd23badad17d7cfd8d22
pasindutennage <pasindunivanthakatennage@gmail.com>
2025-08-20 10:01:52 +0200

    pipeline 10, cft test updated

--- consenbench/scripts/cft-test-automation.sh
+++ consenbench/scripts/cft-test-automation.sh
@@ -8,11 +8,11 @@ mkdir logs
 
 echo "Load,Size,Protocol,Attack,Num_Replicas,Throughput,Latency(ms),Percentile_99(ms),CPU,MEM,NET_IN,NET_OUT"
 
-for protocol in dedis_paxos dedis_raft  racs quepaxa; do # sadl_racs
+for protocol in dedis_paxos dedis_raft quepaxa racs sadl_racs; do #
   /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
 
   size=18
-  for load in 1000 10000 20000 50000 80000 100000 120000 150000 180000 200000 220000 250000 300000 350000 400000; do
+  for load in 1000 10000 20000 30000 50000 80000 100000 120000 150000 180000 200000 220000 250000 300000 350000 400000; do
     /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
   done
 #
@@ -28,15 +28,15 @@ for protocol in dedis_paxos dedis_raft  racs quepaxa; do # sadl_racs
 #
 done
 
-for protocol in efficient; do
-  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
-
-  size=18
-  for load in 1000 10000 20000 50000 80000 100000 120000 150000 180000 200000 220000 250000 300000 350000 400000; do
-    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
-  done
-
-done
+#for protocol in efficient; do
+#  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
+#
+#  size=18
+#  for load in 1000 10000 20000 50000 80000 100000 120000 150000 180000 200000 220000 250000 300000 350000 400000; do
+#    /bin/bash  consenbench/scripts/dry_run.sh   "${protocol}" noop eth1 "$load" "$size"
+#  done
+#
+#done
 
 #for protocol in rabia; do
 #  /bin/bash  consenbench/scripts/dry_setup.sh  "${protocol}" noop eth1
